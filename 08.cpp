b6641ca2b884189585f0b7ba40623978c102f9d5
Andreas Remar <andreas.remar@gmail.com>
2019-12-08 15:29:39 +0100

    Render decoded image

--- 08.cpp
+++ 08.cpp
@@ -12,30 +12,46 @@ struct digits {
 
 int main() {
   auto lines = read_file("08.txt");
-  cout << lines[0].length()/150 << endl;
 
   digits layer[100];
 
   int fewest_zeros = 150;
   int result = -1;
 
+  constexpr int width = 25;
+  constexpr int height = 6;
+
+  int image[width*height];
+
+  for(int i = 0;i < width*height;i++) {
+    image[i] = 2;
+  }
+
   for(int i = 0;i < 100;i++) {
     layer[i].zeros = layer[i].ones = layer[i].twos = 0;
+    int pos = 0;
     for(auto &c : lines[0].substr(i*150, 150)) {
+      int val = -1;
       if(c == '0') {
-	layer[i].zeros++;
+	val = 0;
       } else if(c == '1') {
-	layer[i].ones++;
+	val = 1;
       } else if(c == '2') {
-	layer[i].twos++;
+	val = 2;
       }
-    }
-
-    if(layer[i].zeros < fewest_zeros) {
-      fewest_zeros = layer[i].zeros;
-      result = layer[i].ones * layer[i].twos;
-      cout << result << endl;
+      if(image[pos] == 2) {
+	image[pos] = val;
+      }
+      ++pos;
     }
   }
+
+  for(int y = 0;y < height;y++) {
+    for(int x = 0;x < width;x++) {
+      cout << (image[y*width + x] == 0 ? ' ' : 'X');
+    }
+    cout << endl;
+  }
+
   return 0;
 }
