734081409e3981fbcd90ea58123dafa80b8daacc
Andreas Remar <andreas.remar@gmail.com>
2019-12-14 20:24:28 +0100

    Add trim functions

--- util.h
+++ util.h
@@ -62,4 +62,18 @@ vector<long long int> split_string_to_long_longs(string input) {
   return data;
 }
 
+string& ltrim(string& str, const string& chars = "\t\n\v\f\r ") {
+    str.erase(0, str.find_first_not_of(chars));
+    return str;
+}
+
+string& rtrim(string& str, const string& chars = "\t\n\v\f\r ") {
+    str.erase(str.find_last_not_of(chars) + 1);
+    return str;
+}
+
+string& trim(string& str, const string& chars = "\t\n\v\f\r ") {
+    return ltrim(rtrim(str, chars), chars);
+}
+
 #endif
