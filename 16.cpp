5568d6cd82ed9a08151ce25b26a02072fb650c03
Andreas Remar <andreas.remar@gmail.com>
2019-12-28 08:24:06 +0100

    Solution to part 2 of day 16... easy when you know the pattern.

--- 16.cpp
+++ 16.cpp
@@ -32,18 +32,13 @@ vector<int> phase(vector<int> input) {
   vector<int> output;
 
   int inputSize = input.size();
-  int size = inputSize*10000;
+  int size = inputSize;
 
   for(int i = 0;i < size;i++) {
     PhaseGenerator pg(i + 1);
-    vector<int> totalPhases(inputSize, 0);
-    for(int k = 0;k < size;k++) {
-      totalPhases[k % inputSize] += pg.getPhase();
-    }
-
     int total = 0;
     for(int j = 0;j < inputSize;j++) {
-      total += (input[j] * totalPhases[j]);
+      total += (input[j] * pg.getPhase());
     }
     output.push_back(abs(total) % 10);
   }
@@ -54,22 +49,43 @@ int main() {
   string input = "12345678";
   input = "80871224585914546619083218645595";
   input = "19617804207202209144916044189917";
-  input = real_input();
   input = "03036732577212944063491565474664";
-  cout << input.length() << endl;
+  input = real_input();
+
   vector<int> signal;
 
   for(auto ch : input) {
     signal.push_back(ch - 48);
   }
 
+  vector<int> real_signal;
+  int signalSize = signal.size();
+  for(int i = 0;i < signalSize*10000;i++) {
+    real_signal.push_back(signal[i % signalSize]);
+  }
+
+  int offset = stoi(input.substr(0,7));
+
   for(int i = 0;i < 100;i++) {
     signal = phase(signal);
   }
+  cout << "First part: ";
+  for(int i = 0;i < 8;i++) {
+    cout << signal[i];
+  }
+  cout << endl;
 
-  
-  // for(int i = 5972883;i < 5972883+8;i++) {
-  //   cout << signal[i];
-  // }
-  // cout << endl;
+  for(int j = 0;j < 100;j++) {
+    int sum = real_signal.back();
+    // Overwrite input backwards... and stop at offset
+    for(int i = real_signal.size() - 2;i >= offset;i--) {
+      sum = (sum + real_signal[i]) % 10;
+      real_signal[i] = sum;
+    }
+  }
+  cout << "Second part: ";
+  for(int i = offset;i < offset+8;i++) {
+    cout << real_signal[i];
+  }
+  cout << endl;
 }
